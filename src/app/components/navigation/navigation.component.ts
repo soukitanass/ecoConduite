ac868c0654d0dd4cdb32077fa3a03bbc0e364f3e
Diego <diegocormier@gmail.com>
2019-05-22 15:10:39 -0400

    modification

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -14,8 +14,7 @@ export class NavigationComponent implements OnInit {
 
   mapboxgl;
   map;
-  markerArrival;
-  markerDeparture;
+  makerPosition
   directions;
   displayDirection;
 
@@ -28,9 +27,8 @@ export class NavigationComponent implements OnInit {
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11'
     });
-    this.markerArrival = new this.mapboxgl.Marker()
-    this.markerDeparture = new this.mapboxgl.Marker()
-  
+    this.makerPosition = new this.mapboxgl.Marker()
+
   /***** MAPQUEST*********/
   var MapboxDirection = require('@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions');
   this.directions = new MapboxDirection({
@@ -42,40 +40,7 @@ export class NavigationComponent implements OnInit {
     this.map.addControl(this.directions, 'top-left');
   }
 
-  chosenPlaceDeparture= '';
-  departure;
-  departureLong;
-  departureLat;
 
-  async onEnterDeparture(chosenPlace: string) { 
-    this.chosenPlaceDeparture = chosenPlace;
-    const provider = new OpenStreetMapProvider();
-    this.departure = await provider
-      .search({ query: this.chosenPlaceDeparture });
-
-    this.chosenPlaceDeparture = this.departure[0].label;
-    this.departureLong = this.departure[0].x;
-    this.departureLat = this.departure[0].y;
-
-    this.placeMarker(this.markerDeparture,this.departureLong,this.departureLat)
-  }
-
-  chosenPlaceArrival= '';
-  arrival;
-  arrivalLong;
-  arrivalLat;
-
-   async onEnterArrival(chosenPlace: string) { 
-    this.chosenPlaceArrival = chosenPlace;
-    const provider = new OpenStreetMapProvider();
-    this.arrival = await provider
-    .search({ query: this.chosenPlaceArrival });
-
-    this.chosenPlaceArrival = this.arrival[0].label;
-    this.arrivalLong = this.arrival[0].x;
-    this.arrivalLat = this.arrival[0].y;
-    this.placeMarker(this.markerArrival,this.arrivalLong, this.arrivalLat);
-}
 
 placeMarker(marker,longitude,latitude){
   marker
@@ -86,38 +51,13 @@ placeMarker(marker,longitude,latitude){
   // a changer et recuperer la position 
 }
 
-travel;
- async searchDestination(){
-  this.travel = await this.directions.route({
-    locations: [
-      this.chosenPlaceDeparture,
-      this.chosenPlaceArrival
-    ],
-    maxRoutes: 3,
-    timeOverage: 99
-  })
-
-console.log(this.travel);
-// var lengthArray = this.travel.features.length
-// var latitudeArray :string[];
-// console.log(lengthArray);
-// for(var i; i<lengthArray; i++)
-// {
-//   latitudeArray[i] = this.travel.features[i].properties.latLng.lat;
-//   console.log(latitudeArray[i]);
-// }
-
-}
-
 myPosition(){
 if (navigator.geolocation){
  navigator.geolocation.getCurrentPosition( this.locationFound.bind(this), this.locationNotFound.bind(this));
-}
-
-}
+}}
 
 locationFound(position){
-  this.placeMarker(this.markerDeparture,position.coords.longitude,position.coords.latitude);
+  this.placeMarker(this.makerPosition,position.coords.longitude,position.coords.latitude);
   }
 
 locationNotFound(){
