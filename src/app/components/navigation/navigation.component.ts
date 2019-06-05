7a1c5d6d563e50cfa9f5c83b49dd58858150ddc0
soukainanassib <30549229+soukainanassib@users.noreply.github.com>
2019-06-05 10:33:41 -0400

    Update navigation.component.ts

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+import { Component, OnInit } from '@angular/core';
 import { __await } from 'tslib';
-import { async } from '@angular/core/testing';
 import {} from '@mapbox/mapbox-gl-directions'
 @Component({
   selector: 'app-navigation',
@@ -39,26 +37,19 @@ export class NavigationComponent implements OnInit {
       unit: 'metric',
       profile: 'mapbox/driving-traffic'
     });
-    console.log(this.directions);
     this.map.addControl(this.directions, 'top-left');
     this.map.addControl(new this.mapboxgl.GeolocateControl({
       positionOptions:{enableHighAccuracy: true },
       trackUserLocation: true
     }));
 
-    /*****ELEVATION*********/
-  //   var mapboxElevation = require('mapbox-elevation');
-  //   var getElevation= new mapboxElevation({
-  //     accessToken: 'pk.eyJ1IjoiaGVzdWVjbyIsImEiOiJjanZxcGs0bGUxNWk4M3pyaHIwMHZqcWR1In0.rlzswJuWogDNfb2qy860Ng'
-  //   });
-
- 
-  // getElevation([86.925313, 27.988730], function(err, elevation) {
-  //   console.log('elevation at the summit of mt everest', elevation);
-  // });
+    this.directions.on('route',this.hi.bind(this));
+   
   }
 
-
+  hi(){
+    console.log(this.directions.getWaypoints());
+  }
 
   placeMarker(marker,longitude,latitude){
     marker
