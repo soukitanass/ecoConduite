707f5f1eadb7f834b1f54ed925b2ab0ddfb34d69
soukainanassib <30549229+soukainanassib@users.noreply.github.com>
2019-06-05 11:10:35 -0400

    get origin and destination from directions

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { __await } from 'tslib';
-import { async } from '@angular/core/testing';
 import {} from '@mapbox/mapbox-gl-directions'
 import {} from 'mapbox-elevation'
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
@@ -19,7 +18,6 @@ export class NavigationComponent implements OnInit {
   directions;
   displayDirection;
   getElevation
-  data
   accessToken = 'pk.eyJ1IjoiaGVzdWVjbyIsImEiOiJjanZxcGs0bGUxNWk4M3pyaHIwMHZqcWR1In0.rlzswJuWogDNfb2qy860Ng';
   ngOnInit() {
   /*******SETUP*************/
@@ -42,37 +40,25 @@ export class NavigationComponent implements OnInit {
       unit: 'metric',
       profile: 'mapbox/driving-traffic'
     });
+    this.directions.on('route',this.simulation.bind(this));
     this.map.addControl(this.directions, 'top-left');
     this.map.addControl(new this.mapboxgl.GeolocateControl({
       positionOptions:{enableHighAccuracy: true },
       trackUserLocation: true
     }));
-    //var mapboxElevation = require('mapbox-elevation');
-   // var getElevation = mapboxElevation(this.accessToken);
-  //getElevation([86.925313, 27.988730], function(err, elevation) {
-  //console.log('elevation at the summit of mt everest', elevation);});
+  
   }
   
  simulation(){
-
- let steps =  document.getElementsByClassName("mapbox-directions-step")
-  var lenght = steps.length
-  var departure = steps[0].getAttribute("data-lng")+','+steps[0].getAttribute("data-lat")
-  var arrival = steps[lenght-1].getAttribute("data-lng")+','+steps[lenght-1].getAttribute("data-lat")
+  var departure = this.directions.getOrigin()['geometry'].coordinates;
+  var arrival = this.directions.getDestination()['geometry'].coordinates
   let URL = 'https://api.mapbox.com/directions/v5/mapbox/driving/'+departure+';'+arrival+'?&steps=true&access_token=pk.eyJ1IjoiaGVzdWVjbyIsImEiOiJjanZxcGs0bGUxNWk4M3pyaHIwMHZqcWR1In0.rlzswJuWogDNfb2qy860Ng';
-  console.log(departure)
-  console.log(arrival)
-  console.log(URL)
-  var test =  this.http.get(URL)
+  this.http.get(URL)
     .subscribe(
       data =>{
-        console.log(data);
-        this.data = data;
-        console.log(data['routes'][0].distance);
+        console.log(data['routes'][0].legs[0].steps);
       },
       (error)=>{console.log('erreur'+error)});
-  console.log("Lancement de la simulation")
-
 }
 
   placeMarker(marker,longitude,latitude){
